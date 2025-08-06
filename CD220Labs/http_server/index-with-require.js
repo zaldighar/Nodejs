23ae01bb65f51e55bf311415f64a7c89db8add9c
zaldighar <zaldighar@outlook.com>
2025-08-06 16:04:59 -0500

    Refactor code formatting for consistency in index-with-require.js and today.js

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -1,14 +1,14 @@
- // Import the HTTP module
-const http = require('http');
+// Import the HTTP module
+const http = require("http");
 
 // Import the 'today' module
-const today = require('./today');
+const today = require("./today");
 
 // Define the request listener function
 const requestListener = function (req, res) {
-    res.writeHead(200); // Set the status code to 200 (OK)
-    // Send the response with the current date from the 'today' module
-    res.end(`Hello, World! The date today is ${today.getDate()}`);
+  res.writeHead(200); // Set the status code to 200 (OK)
+  // Send the response with the current date from the 'today' module
+  res.end(`Hello, World! The date today is ${today.getDate()}`);
 };
 
 // Define the port number
@@ -19,4 +19,4 @@ const server = http.createServer(requestListener);
 
 // Start the server and listen on the specified port
 server.listen(port);
-console.log('Server listening on port: ' + port);
+console.log("Server listening on port: " + port);
--- CD220Labs/http_server/today.js
+++ CD220Labs/http_server/today.js
@@ -1,6 +1,8 @@
- // Export a function named 'getDate' from the module
+// Export a function named 'getDate' from the module
 module.exports.getDate = function getDate() {
-    // Get the current date and time in the timezone "Australia/Brisbane"
-    let aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
-    return aestTime; // Return the formatted date and time
+  // Get the current date and time in the timezone "Australia/Brisbane"
+  let aestTime = new Date().toLocaleString("en-US", {
+    timeZone: "Australia/Brisbane",
+  });
+  return aestTime; // Return the formatted date and time
 };
